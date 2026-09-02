7afc47a479405c1a5a0862598513dbd47c8f75a2
Riot-Mind-Studios-LLC <avelazquez585@gmail.com>
2026-09-01 20:09:08 -0400

    added more details to useMemo

--- src/data/detailData.js
+++ src/data/detailData.js
@@ -956,9 +956,126 @@ const detail = [
     ╚██████╔╝███████║███████╗██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║╚██████╔╝
      ╚═════╝ ╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ 
 
+    // ============================================
+    // React useMemo Hook
+    // ============================================
+    //
+    // The React useMemo Hook returns a memoized value.
+    // Think of memoization as caching a value so that it does not need to be recalculated.
+    // The useMemo Hook only runs when one of its dependencies update.
+    // This can improve performance.
+    //
+    // The useMemo and useCallback Hooks are similar:
+    //  - useMemo returns a memoized value.
+    //  - useCallback returns a memoized function.
+    //
+    // WITHOUT USEMEMO
+    //
+    // The useMemo Hook can be used to keep expensive, resource intensive functions from needlessly running.
+    // In this example, we have an expensive function that runs on every render.
+    // When changing the count or adding a todo, you will notice a delay in execution.
+    //
+    // Example: A poor performing function. The expensiveCalculation function runs on every render:
+    import { useState } from 'react';
+    import { createRoot } from 'react-dom/client';
+
+    const App = () => {
+      const [count, setCount] = useState(0);
+      const [todos, setTodos] = useState([]);
+      const calculation = expensiveCalculation(count);
+
+      const increment = () => {
+        setCount((c) => c + 1);
+      };
+      const addTodo = () => {
+        setTodos((t) => [...t, "New Todo"]);
+      };
+
+      return (
+        <div>
+          <div>
+            <h2>My Todos</h2>
+            {todos.map((todo, index) => {
+              return <p key={index}>{todo}</p>;
+            })}
+            <button onClick={addTodo}>Add Todo</button>
+          </div>
+          <hr />
+          <div>
+            Count: {count}
+            <button onClick={increment}>+</button>
+            <h2>Expensive Calculation</h2>
+            {calculation}
+            <p>Note that this example executes the expensive function also when you click on the Add Todo button.</p>
+          </div>
+        </div>
+      );
+    };
+
+    // runs on every render
+    const expensiveCalculation = (num) => {
+      console.log("Calculating...");
+      for (let i = 0; i < 1000000000; i++) {
+        num += 1;
+      }
+      return num;
+    };
+
+    createRoot(document.getElementById('root')).render(
+      <App />
+    );
+    //
+    // USE USEMEMO
+    //
+    // To fix this performance issue, we can use the useMemo Hook to memoize the expensiveCalculation function. This will cause the function to only run when needed.
+    // We can wrap the expensive function call with useMemo.
+    // The useMemoHook accepts a second parameter to declare dependencies. The expensive function will only run when its dependencies have changed.
+    // In the following example, the expensive function will only run when count is changed and not when todo's are added.
+    //
+    // Example: Performance example using the useMemo Hook:
+    const App = () => {
+      const [count, setCount] = useState(0);
+      const [todos, setTodos] = useState([]);
+      const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+      const increment = () => {
+        setCount((c) => c + 1);
+      };
+      const addTodo = () => {
+        setTodos((t) => [...t, "New Todo"]);
+      };
+
+      return (
+        <div>
+          <div>
+            <h2>My Todos</h2>
+            {todos.map((todo, index) => {
+              return <p key={index}>{todo}</p>;
+            })}
+            <button onClick={addTodo}>Add Todo</button>
+          </div>
+          <hr />
+          <div>
+            Count: {count}
+            <button onClick={increment}>+</button>
+            <h2>Expensive Calculation</h2>
+            {calculation}
+          </div>
+        </div>
+      );
+    };
+
+    const expensiveCalculation = (num) => {
+      console.log("Calculating...");
+      for (let i = 0; i < 1000000000; i++) {
+        num += 1;
+      }
+      return num;
+    };
+
     // 1. takes two arguments:
-        // a function that returns a value,
-        // and a dependency array (same concept as useEffect's dependency array).
+      // a function that returns a value,
+      // and a dependency array (same concept as useEffect's dependency array).
 
     // 2. The function runs and its return value gets cached.
 
@@ -1001,8 +1118,6 @@ const detail = [
         </div>
       );
     }
-
-    
 
     /*
       The core question useMemo answers is: "is this calculation expensive enough that recalculating
